e9f307ac3a346ada13af3f779afd2692bdf22eb3
Olatunji Oladele <olatunji.oladele@cwlgroup.com>
2016-05-17 16:57:49 +0100

    Done!

--- quiz.js
+++ quiz.js
@@ -11,4 +11,33 @@ function NotesApplication(author){
 		}
 			
 	}
+	this.listNotes= function(){
+		if(this.noteArray.length < 1){
+			console.log("No notes.");
+		}
+		else{
+			for(let i=0; i<this.notes.length;i++){
+				console.log("Note ID: " + this.noteArray[i]);
+				console.log(this.notes[i]);
+				console.log("\n\nBy Author " + this.author + "\n");
+			}
+		}
+	}
+	this.get=function (note_id){
+		return "Note ID: "+ this.notes[note_id];
+	}
+	this.search=function(search_text){
+		if(this.notes.includes(search_text)){
+			return "Note ID: "+ this.note_id+"\n"+note_content+"\n\n"+ "By Author " +this.author;
+		}
+		else{
+			return "No result."
+		}
+	}
+	this.delete= function(note_id){
+		delete this.notes[note_id]
+	}
+	this.edit=function(note_id, new_content){
+		this.notes[note_id]=new_content;
+	}
 }
