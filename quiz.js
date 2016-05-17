62a6f39aaf155050e47c49550f8d947535ce4b87
Olatunji Oladele <olatunji.oladele@cwlgroup.com>
2016-05-17 17:05:43 +0100

    add edit function

--- quiz.js
+++ quiz.js
@@ -3,14 +3,14 @@ function NotesApplication(author){
 	this.notes=[];
 
 	this.create= function (note_content){
-		if note_content.length>0
-			this.notes.push(note_content)
-			return "Note created!"
+		if (note_content.length>0){
+			this.notes.push(note_content);
+			return "Note created!"}
 		else {
-			return "Note not created!"
+			return "Note not created!";
 		}
 			
-	}
+	};
 	this.listNotes= function(){
 		if(this.noteArray.length < 1){
 			console.log("No notes.");
@@ -22,22 +22,22 @@ function NotesApplication(author){
 				console.log("\n\nBy Author " + this.author + "\n");
 			}
 		}
-	}
+	};
 	this.get=function (note_id){
 		return "Note ID: "+ this.notes[note_id];
-	}
+	};
 	this.search=function(search_text){
 		if(this.notes.includes(search_text)){
 			return "Note ID: "+ this.note_id+"\n"+note_content+"\n\n"+ "By Author " +this.author;
 		}
 		else{
-			return "No result."
+			return "No result.";
 		}
-	}
+	};
 	this.delete= function(note_id){
-		delete this.notes[note_id]
-	}
+		delete this.notes[note_id];
+	};
 	this.edit=function(note_id, new_content){
 		this.notes[note_id]=new_content;
-	}
+	};
 }
